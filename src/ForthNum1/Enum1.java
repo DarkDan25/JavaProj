5f5d6f9bd5ed6785fa38926837ed94c83e72a4cd
Zyablik <elektrosha88@gmial.com>
2023-09-21 10:47:35 +0300

    Add method infoSeason and TestEnum.java

--- src/ForthNum1/Enum1.java
+++ src/ForthNum1/Enum1.java
@@ -7,4 +7,15 @@ public class Enum1 {
         SUMMER,
         AUTUMN
     }
+    public String infoSeason(Seasons season)
+    {
+        return switch (season) {
+            case WINTER -> "I like winter";
+            case SPRING -> "I like spring";
+            case SUMMER -> "I like summer";
+            case AUTUMN -> "I like autumn";
+            default -> "Wrong season";
+        };
+
+    }
 }
---
+++ src/ForthNum1/TestEnum.java
@@ -0,0 +1,9 @@
+package ForthNum1;
+
+public class TestEnum {
+
+    public static void main(String[] args) {
+        Enum1 num = new Enum1();
+        System.out.println(num.infoSeason(Enum1.Seasons.SUMMER));
+    }
+}
