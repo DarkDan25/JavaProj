a50b59895c71cd0894c0d06e5e68192766d2c9bf
Zyablik <elektrosha88@gmial.com>
2023-09-21 11:00:22 +0300

    Add sort for array in Randoms.java

--- src/ThirdNum1_5/Randoms.java
+++ src/ThirdNum1_5/Randoms.java
@@ -15,9 +15,26 @@ public class Randoms {
             {
                 arr[i]= rand.nextInt(n+1);
             }
+            int chetCounter =0; //Check how many even numbers
+            System.out.println("Original array");
             for(int i=0;i<n;i++)
             {
+                if(arr[i]%2==0) chetCounter++;
                 System.out.print(arr[i]);
+            }
+            int[] arr2 = new int[chetCounter];
+            int f =0; //Index of new array
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]%2 ==0 && arr[i]!=0)
+                {
+                    arr2[f] = arr[i];
+                    f++;
+                }
+            }
+            System.out.println("\nSorted array");
+            for(int i=0;i<chetCounter;i++){
+                System.out.print(arr2[i]);
             }
         }
         else{
