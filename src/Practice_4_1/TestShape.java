75b1545f4b93fbfb9e46be7aa4a65f834953c8d3
Zyablik <elektrosha88@gmial.com>
2023-09-28 14:44:44 +0300

    Little fix of TestShape.java

--- src/Practice_4_1/TestShape.java
+++ src/Practice_4_1/TestShape.java
@@ -1,13 +1,12 @@
 package Practice_4_1;
-import java.util.Scanner;
 public class TestShape {
 
     public static void main(String[] args) {
         Shape sh1 = new Circle(3);
         Shape sh2 = new Square(4);
         Shape sh3 = new Rectangle(3,2);
-        System.out.println(sh1.toString());
-        System.out.println(sh2.toString());
-        System.out.println(sh3.toString());
+        System.out.println(sh1);
+        System.out.println(sh2);
+        System.out.println(sh3);
     }
 }
