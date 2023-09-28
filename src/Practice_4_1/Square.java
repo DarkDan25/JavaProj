805aaa0561e2a0680fb0c481c5b554b54cc074c0
Zyablik <elektrosha88@gmial.com>
2023-09-28 14:37:23 +0300

    Made full class Square

--- src/Practice_4_1/Square.java
+++ src/Practice_4_1/Square.java
@@ -1,7 +1,30 @@
 package Practice_4_1;
 
 public class Square extends Shape{
-    public Square() {
+    String name = "Square";
+    int a;
+    public Square(int a) {
         super();
+        this.a=a;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4*a;
+    }
+
+    @Override
+    public String getType() {
+        return name;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(a,2);
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString()+ getType() + "\nIt's area is "+ getArea()+ "\nIt's perimeter is "+getPerimeter());
     }
 }
--- src/Practice_4_1/TestShape.java
+++ src/Practice_4_1/TestShape.java
@@ -4,9 +4,10 @@ public class TestShape {
 
     public static void main(String[] args) {
         Shape sh1 = new Circle(3);
-        Shape sh2 = new Square();
+        Shape sh2 = new Square(4);
         Shape sh3 = new Rectangle(3,2);
         System.out.println(sh1.toString());
+        System.out.println(sh2.toString());
         System.out.println(sh3.toString());
     }
 }
