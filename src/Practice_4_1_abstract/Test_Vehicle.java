9008aa2ada5bf57c2480234990b8dbbbfe1efc6c
Zyablik <elektrosha88@gmial.com>
2023-10-04 10:42:40 +0300

    Add some functions to Test_Vehicle.java

--- src/Practice_4_1_abstract/Test_Vehicle.java
+++ src/Practice_4_1_abstract/Test_Vehicle.java
@@ -4,5 +4,10 @@ package Practice_4_1_abstract;
 public class Test_Vehicle {
     public static void main(String[] args) {
         Car c = new Car(100,50);
+        Train t = new Train(2374,120);
+        c.CostOfRoad();
+        t.setNumOfStops(4);
+        t.timeOfTrip();
+        t.CostOfRoad();
     }
 }
