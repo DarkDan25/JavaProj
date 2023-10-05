a587548fd1b92297bdb503acbdae561e89b9b0b4
Zyablik <elektrosha88@gmial.com>
2023-10-05 11:19:02 +0300

    Create Score table

--- src/Pract_5/Milan_vs_Madrid.java
+++ src/Pract_5/Milan_vs_Madrid.java
@@ -1,8 +1,5 @@
 package Pract_5;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class Milan_vs_Madrid {
     public static void main(String[] args) {
         MyFrame frame = new MyFrame();
--- src/Pract_5/MyFrame.java
+++ src/Pract_5/MyFrame.java
@@ -1,20 +1,64 @@
 package Pract_5;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
 
 public class MyFrame extends JFrame{
+    int  ScoreMilan, ScoreNadrid = 0;
+    public void Winner(int Milan, int Madrid, JLabel winner)
+    {
+        if(Milan>Madrid)
+        {
+            winner.setText("Winner: AC Milan");
+        }
+        else if(Milan < Madrid)
+        {
+            winner.setText("Winner: Real Madrid");
+        }
+        else{
+            winner.setText("Winner: DRAW");
+        }
+    }
     public MyFrame()
     {
         setSize(600,400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JButton b = new JButton("Hi");
-        b.addActionListener( add_New ->
+
+        JPanel panel= new JPanel();
+        JButton Milan = new JButton("AC Milan");
+        JButton Madrid = new JButton("Real Madrid");
+        JLabel CurScore = new JLabel("Result: "+ScoreMilan+" X "+ScoreNadrid, JLabel.LEFT);
+        JLabel CurGoal = new JLabel("Last scorer: N/A", JLabel.RIGHT);
+        JLabel Winner = new JLabel("Winner: N/A", JLabel.CENTER);
+        panel.setBackground(Color.WHITE);
+        panel.setPreferredSize(new Dimension(500,300));
+
+
+        Milan.addActionListener( add_New ->
                 {
-                    System.out.println("hi");
+                    ScoreMilan++;
+                    CurScore.setText("Result: "+ScoreMilan+" X "+ScoreNadrid);
+                    Winner(ScoreMilan,ScoreNadrid,Winner);
+                    CurGoal.setText("Last scorer: AC Milan");
                 }
         );
-        getContentPane().add(b);
+
+        Madrid.addActionListener( add_new ->
+                {
+                    ScoreNadrid++;
+                    CurScore.setText("Result: "+ScoreMilan+" X "+ScoreNadrid);
+                    Winner(ScoreMilan,ScoreNadrid,Winner);
+                    CurGoal.setText("Last scorer: Real Madrid");
+                }
+
+        );
+        getContentPane().add(panel);
+        panel.add(CurScore);
+        panel.add(CurGoal);
+        panel.add(Winner);
+        panel.add(Milan);
+        panel.add(Madrid);
+        pack();
         setVisible(true);
     }
 }
